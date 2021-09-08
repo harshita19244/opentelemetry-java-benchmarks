9a0a7c92d6a9757180737052a23635045db4720a
Harshita Srinivas <60603854+harshita19244@users.noreply.github.com>
2021-09-09 01:19:33 +0530

    Update runner.sh

--- runner/runner.sh
+++ runner/runner.sh
@@ -6,14 +6,20 @@ RESULTS_DIR=${RESULTS_DIR:-/results}
 export JAVA_HOME=/usr/lib/jvm/jre-openjdk
 
 echo "🏃 Starting Jaeger"
-/tmp/jaeger-1.25.0-linux-amd64/jaeger-all-in-one --memory.max-traces 1_000_000 >"${RESULTS_DIR}/jaeger-${test}.log" 2>&1 &
-pid=$!
+/tmp/jaeger-1.25.0-linux-amd64/jaeger-all-in-one --memory.max-traces 10 >"${RESULTS_DIR}/jaeger-${test}.log" 2>&1 &pid=$!
 if [ $? != 0 ]; then
     echo "🛑 Failed to start Jaeger"
     exit 1
 fi
 
-for test in spring-boot simple-java spring-cloud; do
+echo "🏃 Starting Collector"
+/tmp/opentelemetry-collector-0.34.0-linux-amd64/otel-col --config ./tmp/otel-config.yaml >"${RESULTS_DIR}/otlp-${test}.log" 2>&1 &pid2=$!
+if [ $? != 0 ]; then
+    echo "🛑 Failed to start Collector"
+    exit 1
+fi
+
+for test in simple-java; do
     cd "${WORK_DIR}/opentelemetry-benchmark-${test}"
     if [ $? != 0 ]; then
         echo "🛑 Failed to find the location for the test '${test}'"
@@ -47,55 +53,6 @@ for test in spring-boot simple-java spring-cloud; do
     echo "✅ ${test} - Done!"
 done
 
-for test in jdbc jaxrs java-servlet-filter; do
-    cd "${WORK_DIR}/opentelemetry-benchmark-${test}"
-    if [ $? != 0 ]; then
-        echo "🛑 Failed to find the location for the test '${test}'"
-        continue
-    fi
-
-    echo "🩹 ${test} - Manually patching pom.xml to build with Java 11"
-    sed 's/<javac.target>14/<javac.target>11/gi' -i pom.xml
-    if [ $? != 0 ]; then
-        echo "🛑 Failed to patch the test '${test}'"
-        continue
-    fi
-
-    echo "⚒️ ${test} - Building"
-    mvn clean install -l "${RESULTS_DIR}/build-${test}.log"
-    if [ $? != 0 ]; then
-        echo "🛑 Failed to build the test '${test}'"
-        continue
-    fi
-
-    echo "🏃 Running ${test}"
-    java -javaagent:"${AUTO_AGENT}" \
-        -Dotel.resource.attributes=service.name="benchmark" \
-        -Dotel.traces.exporter=jaeger -Dotel.metrics.exporter=jaeger \
-        -Dotel.exporter.jaeger.endpoint=http://localhost:14250/ \
-        -jar target/benchmarks.jar >"${RESULTS_DIR}/benchmark-${test}-jaeger.log" 2>&1
-
-    java -javaagent:"${AUTO_AGENT}" \
-        -Dotel.resource.attributes=service.name="benchmark" \
-        -Dotel.traces.exporter=otlp \
-        -Dotel.metrics.exporter=otlp \
-        -Dotel.exporter.jaeger.endpoint=http://localhost:4317/ \
-        -jar target/benchmarks.jar >"${RESULTS_DIR}/benchmark-${test}-otlp.log" 2>&1
-
-    java -javaagent:"${AUTO_AGENT}" \
-        -Dotel.resource.attributes=service.name="benchmark" \
-        -Dotel.metrics.exporter=none \
-        -jar target/benchmarks.jar >"${RESULTS_DIR}/benchmark-${test}-notracer.log" 2>&1
-
-    if [ $? != 0 ]; then
-        echo "🛑 Failed to run the test '${test}'. Skipping next iterations."
-        break
-    fi
-    echo "✔️ ${test} done. Waiting about 1 minute for the machine to settle down."
-    sleep 1m
-
-    echo "✅ ${test} - Done!"
-done
 
 echo "🎣 Retrieving Jaeger metrics"
 curl -so "${RESULTS_DIR}/jaeger-metrics.log" http://localhost:14269/metrics
@@ -105,5 +62,6 @@ if [ $? != 0 ]; then
 fi
 
 echo "🏃 Stopping Jaeger"
-kill "${pid}"
-wait ${pid}
+kill -9 ${pid}
+echo "🏃 Stopping Collector"
+kill -9 ${pid2}
